c565911a4dd697b09a00d25da16fedc2880166d9
petersamson-kingori <peters.kingori@moringaschool.com>
2023-06-12 18:10:16 +0300

    controller test

--- api/app/controllers/welcomes_controller.rb
+++ api/app/controllers/welcomes_controller.rb
@@ -1,7 +1,7 @@
 class WelcomesController < ApplicationController
 
     def index
-        render json: { message: 'Hello, index world! This is our development branch' }
+        render json: { message: 'Hello,world! This is our development branch' }
       end
       
 end
