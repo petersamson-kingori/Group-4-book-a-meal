d875d29983e07c95557fa2840368b69724150fc1
petersamson-kingori <peters.kingori@moringaschool.com>
2023-06-20 14:51:23 +0300

    jwt-auth

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,9 @@
 
 Rails.application.routes.draw do
-  root 'welcomes#index'
+ 
   namespace :api do
     namespace :v1 do
+      root 'welcomes#index'
       resources :welcomes
       resources :users, only: [:create, :index]
       post '/login', to: 'auth#create'
