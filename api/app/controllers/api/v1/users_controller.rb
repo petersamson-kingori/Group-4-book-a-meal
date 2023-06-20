1e8c602cab8697746d966fe0ab72c9a5bbafc704
petersamson-kingori <peters.kingori@moringaschool.com>
2023-06-20 15:46:37 +0300

    jwt-auth

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -25,6 +25,6 @@ class Api::V1::UsersController < ApplicationController
     private
   
     def user_params
-      params.require(:user).permit(:username, :password)
+      params.require(:user).permit(:username, :password, :password_confirmation)
     end
   end
