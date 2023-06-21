a88b31b1e5e0f4ea0a48a96953d287f363a0a520
petersamson-kingori <peters.kingori@moringaschool.com>
2023-06-21 12:51:41 +0300

    cors

--- api/app/controllers/api/v1/auth_controller.rb
+++ api/app/controllers/api/v1/auth_controller.rb
@@ -2,14 +2,14 @@ class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
   
     def create
-      @user = User.find_by(email: user_login_params[:email])
+      @user = User.find_by(username: user_login_params[:username])
       #User#authenticate comes from BCrypt
       if @user && @user.authenticate(user_login_params[:password])
         # encode token comes from ApplicationController
         token = encode_token({ user_id: @user.id })
         render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
       else
-        render json: { message: 'Invalid email or password' }, status: :unauthorized
+        render json: { message: 'Invalid username or password' }, status: :unauthorized
       end
     end
 
@@ -24,7 +24,7 @@ class Api::V1::AuthController < ApplicationController
     private
   
     def user_login_params
-      # params { user: {email: 'Chandler Bing', password: 'hi' } }
-      params.require(:user).permit(:email, :password)
+      # params { user: {username: 'Chandler Bing', password: 'hi' } }
+      params.require(:user).permit(:username, :password)
     end
   end
