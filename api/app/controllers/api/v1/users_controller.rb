e6da4cebd8367657c68aa9e184eaae57e1ee487d
petersamson-kingori <peters.kingori@moringaschool.com>
2023-06-29 02:05:20 +0300

    show users

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -27,14 +27,25 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def current_user
-      render json: { user: UserSerializer.new(current_user) }, status: :ok
+      user_id = decode_token(request.headers['Authorization']) # Replace with your JWT decoding logic
+      @current_user ||= User.find(user_id)
     end
-
 
       
     private
+
+    
   
     def user_params
       params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    end
+
+    def decode_token(token)
+      # Implement your JWT decoding logic to retrieve the user ID from the token
+      # This is just a placeholder, you need to replace it with your actual decoding logic
+      decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
+      decoded_token.first['user_id']
+    rescue JWT::DecodeError
+      nil
     end
   end
