843c810f503caae35e26194b447687ad4632d882
petersamson-kingori <peters.kingori@moringaschool.com>
2023-07-04 17:55:31 +0300

    menu & options

--- api/app/serializers/caterer_serializer.rb
+++ api/app/serializers/caterer_serializer.rb
@@ -1,4 +1,22 @@
 class CatererSerializer < ActiveModel::Serializer
   attributes :id, :username, :business_name
   has_many :menus, include_nested_association: true
+  
+  def include_menus?
+    object.menus.present?
+  end
+
+  def menus
+    object.menus.map do |menu|
+      {
+        id: menu.id,
+        name: menu.name,
+        description: menu.description,
+        menu_options: menu.menu_options
+      }
+    end
+  end
+  
 end
+
+
