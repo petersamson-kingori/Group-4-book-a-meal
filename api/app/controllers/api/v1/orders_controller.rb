2bcd58f1a7d28b45298913283c2364d5b7aa5dd5
petersamson-kingori <peters.kingori@moringaschool.com>
2023-07-10 02:20:58 +0300

    Orders controller

--- api/app/controllers/api/v1/orders_controller.rb
+++ api/app/controllers/api/v1/orders_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::OrdersController < ApplicationController
     skip_before_action :authorized, only: [:index, :create]
     def create
-      user_id = params[:userId]
-      items = params[:items]
-      shipping_location = params[:shippingLocation]
-  
+      user_id = order_params[:userId]
+      items = order_params[:items]
+      shipping_location = order_params[:shippingLocation]
+    
       # Assuming you have a model called MenuOption
       # Create menu option records for each item in the order
       menu_options = items.map do |item|
@@ -14,24 +14,24 @@ class Api::V1::OrdersController < ApplicationController
           price: item[:price]
         }
       end
-  
+    
       # Save the order details and associated menu options
-      if Order.create(user_id: user_id, shipping_location: shipping_location, menu_options: menu_options)
+      order = Order.new(user_id: user_id, shipping_location: shipping_location)
+      order.menu_options.build(menu_options)
+    
+      if order.save
         render json: { message: 'Order submitted successfully' }, status: :created
       else
-        render json: { error: 'Failed to submit order' }, status: :unprocessable_entity
+        render json: { error: 'Failed to submit order', errors: order.errors.full_messages }, status: :unprocessable_entity
       end
     end
   
-    def index
-      orders = Order.all
-      render json: orders
-    end
+    # Other actions...
   
     private
   
     def order_params
-      params.permit(:userId, items: [:id, :name, :price], :shippingLocation)
+      params.require(:order).permit(:userId, items: [:id, :name, :price], :shippingLocation)
     end
   end
   
