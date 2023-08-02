a50cf7615289e4ad0885a02ac9628889e081bb55
petersamson-kingori <peters.kingori@moringaschool.com>
2023-08-02 20:32:11 +0300

    submit test

--- api/app/controllers/api/v1/orders_controller.rb
+++ api/app/controllers/api/v1/orders_controller.rb
@@ -2,36 +2,26 @@ class Api::V1::OrdersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    @order = Order.new(user_id: order_params[:userId], email: order_params[:email], shipping_location: order_params[:shippingLocation])
-
-    begin
-      Order.transaction do
-        @order.save!
-
-        # Create OrderItems for each item
-        params[:items].each do |item|
-          menu_option = MenuOption.find_by(id: item[:id])
-          if menu_option
-            @order.order_items.create!(menu_option: menu_option)
-          else
-            render json: { error: "Menu option with id #{item[:id]} not found" }, status: :unprocessable_entity and return
-          end
-        end
+    @order = Order.new(order_params.except(:items))
+  
+    if @order.save
+      # Create OrderItems for each item
+      params[:items].each do |item|
+        @order.order_items.create(menu_option_id: item[:id])
       end
-
       render json: @order, status: :created, location: @order
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { errors: e.record.errors }, status: :unprocessable_entity
+    else
+      render json: @order.errors, status: :unprocessable_entity
     end
-
+  end
+  
     
   
 
   private
 
-  def order_params
-    params.permit(:userId, :email, :shippingLocation, items: [:id, :name, :price])
-  end
-  
-  
+def order_params
+  params.require(:order).permit(:userId, :email, :shippingLocation, items: [:id])
+end
+
 end
