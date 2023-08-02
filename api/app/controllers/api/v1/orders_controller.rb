e59e70c07d914b2b85856ab8419b8419c9018fd4
petersamson-kingori <peters.kingori@moringaschool.com>
2023-08-02 20:01:12 +0300

    submit test

--- api/app/controllers/api/v1/orders_controller.rb
+++ api/app/controllers/api/v1/orders_controller.rb
@@ -3,21 +3,23 @@ class Api::V1::OrdersController < ApplicationController
 
   def create
     @order = Order.new(user_id: order_params[:userId], email: order_params[:email], shipping_location: order_params[:shippingLocation])
-
+  
     begin
       Order.transaction do 
         @order.save!
-
+  
         # Create OrderItems for each item
         params[:items].each do |item|
-          if MenuOption.exists?(item[:id])
-            @order.order_items.create!(menu_option_id: item[:id])
+          menu_option = MenuOption.find_by(id: item[:id])
+  
+          if menu_option
+            @order.order_items.create!(menu_option: menu_option)
           else
             render json: { error: "Menu option with id #{item[:id]} not found" }, status: :unprocessable_entity and return
           end
         end
       end
-
+  
       render json: @order, status: :created, location: @order
     rescue ActiveRecord::RecordInvalid => e
       render json: { errors: e.record.errors }, status: :unprocessable_entity
