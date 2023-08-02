e14508cc262a9fb9bab265d58fe25170b301f87d
petersamson-kingori <peters.kingori@moringaschool.com>
2023-08-02 20:06:25 +0300

    submit test

--- api/app/controllers/api/v1/orders_controller.rb
+++ api/app/controllers/api/v1/orders_controller.rb
@@ -1,34 +1,34 @@
 class Api::V1::OrdersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
-  def create
+def create
     @order = Order.new(user_id: order_params[:userId], email: order_params[:email], shipping_location: order_params[:shippingLocation])
-  
+
+    # Adding begin/rescue to handle error
     begin
       Order.transaction do 
         @order.save!
-  
+
         # Create OrderItems for each item
         params[:items].each do |item|
-          menu_option = MenuOption.find_by(id: item[:id])
-  
-          if menu_option
-            @order.order_items.create!(menu_option: menu_option)
-          else
-            render json: { error: "Menu option with id #{item[:id]} not found" }, status: :unprocessable_entity and return
-          end
+          @order.order_items.create!(menu_option_id: item[:id])
         end
       end
-  
+
       render json: @order, status: :created, location: @order
+    # Here we handle the error and return a more explicit error message
     rescue ActiveRecord::RecordInvalid => e
       render json: { errors: e.record.errors }, status: :unprocessable_entity
     end
-  end
+end
+
+    
   
+
   private
 
   def order_params
     params.permit(:userId, :email, :shippingLocation, items: [:id])
   end
+  
 end
